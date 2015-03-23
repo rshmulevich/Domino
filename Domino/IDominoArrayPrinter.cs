c67385f1e00ffcb153e7f97c10a5406a3a5c289c
msxor <mshmulevich@gmail.com>
2015-03-22 22:45:06 -0400

    added interface to implement

---
+++ Domino/IDominoArrayPrinter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domino
+{
+    internal interface IDominoArrayPrinter
+    {
+        void Print(IEnumerable<JPiece> pieces, string delimiter ); // "->"
+    }
+}
